292b5943231bdc649f552b56ba5e234015abdc5e
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-02-20 21:57:01 -0300

    alterado input de date para text, esta dando a quantidade de dias faltantes corretamente.

--- function.js
+++ function.js
@@ -1,14 +1,25 @@
-function alarme () {
-    let birthday = parseInt(document.getElementById("birthday")).value;
-    let day = parseInt(document.getElementById("day")).value;
+/* function alarme () {
+    let birthday = parseInt(document.getElementById("birthday").value);
+    let day = parseInt(document.getElementById("day").value);
     let daysLeft = day - birthday;
     let timeLeft = daysLeft / (1000*60*60*24);
 
-    if(dayLeft == 0){
-        alert("feliz aniversario.")
-    } else {
+    if(daysLeft > 0){
         alert(`Falta, ${daysLeft} dias.`)
+    } else {
+        alert("feliz aniversario.")
     }
     console.log(timeLeft);
     console.log(daysLeft);
+} */
+function alarme () {
+let birthday = parseInt(document.getElementById("birthday").value);
+let day = parseInt(document.getElementById("day").value);
+const dias =  new Date(birthday) -new Date(day)
+const diffInDays = dias / (1000*60*60*24);
+if (diffInDays > 0){
+    console.log(`Falta ${diffInDays} dias`);
+}else {
+    console.log("Feliz aniversario.");
+}
 }
